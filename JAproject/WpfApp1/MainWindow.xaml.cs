458f9f17d4cbf3128269e595f95e6045af3404b3
MaciejMaciejewski <maciekageof@gmail.com>
2022-10-22 18:01:53 +0200

    first working asm

---
+++ JAproject/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            [DllImport(@"C:\Users\Macie\JA\JAproject\JAproject\WpfApp1\x64\Debug\ASMdll.dll")]
+            static extern int MyProc1(int a, int b);
+            int x = 12;
+            int y = 5;
+            int retVal = MyProc1(x, y);
+            textBox.Text = retVal.ToString();
+        }
+    }
+}
